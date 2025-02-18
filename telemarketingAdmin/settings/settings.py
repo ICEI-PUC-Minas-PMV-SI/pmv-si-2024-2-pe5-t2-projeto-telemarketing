985c27776d0dfc91a89499d545201889ef5948bb
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2025-02-18 20:22:02 -0300

    Improvements in _base.htm, blog.html and list_client.html

--- telemarketingAdmin/settings/settings.py
+++ telemarketingAdmin/settings/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xhx&jvg4exd+^$ik*q77_9e=(vr5dhpp084ew&a*sraw5#&1h&"
+# SECRET_KEY = "django-insecure-xhx&jvg4exd+^$ik*q77_9e=(vr5dhpp084ew&a*sraw5#&1h&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
